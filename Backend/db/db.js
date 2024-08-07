f5c2fab8e311d75ecb506a256bf9936846757565
Yash Nishankar <yashnishankar3102001@gmail.com>
2024-08-07 16:24:02 +0530

    done

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,20 +2,45 @@ const mysql = require('mysql2');
 
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "react news app"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 
 conn.connect((error) => {
     if (error) {
-        console.log("error", error);
+        console.log("Error connecting to the database:", error);
+        process.exit(1);
     } else {
         console.log("successfully connected");
     }
 })
+
+
+conn.on('error', (err) => {
+    console.error('Database connection error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        // Reconnect on connection loss
+        reconnect();
+    } else {
+        // Exit on other errors
+        process.exit(1);
+    }
+});
+
+const reconnect = () => {
+    console.log('Attempting to reconnect to the database...');
+    conn.connect((err) => {
+        if (err) {
+            console.error('Error reconnecting to the database:', err);
+            setTimeout(reconnect, 2000); // Retry connection after 2 seconds
+        } else {
+            console.log('Successfully reconnected to the database');
+        }
+    });
+};
 
 
 module.exports = conn;
